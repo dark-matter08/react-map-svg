c3f9b8b5150779a04c94f5c2f0f1312077d41b69
Nde Lucien <38033535+dark-matter08@users.noreply.github.com>
2022-12-07 18:19:25 +0100

    removed unneccesary

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 
-import { ChartMap, Map } from "./components";
+import { ChartMap } from "./components";
 
 function App() {
   const empl_data = [
@@ -43,21 +43,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header h-screen">
-        {/* <div className="w-[20%] h-screen overflow-y-scroll">
-          {map_list.map((country, index) => {
-            const country_name = country[1];
-            return (
-              <div
-                className="bg-[#442312] hover:bg-[#272d11] py-1 my-2 rounded-md hover:cursor-pointer"
-                onClick={() => {
-                  setCountry(country);
-                }}
-              >
-                {country_name}
-              </div>
-            );
-          })}
-        </div> */}
         <ChartMap marker_data={empl_data} projection="Miller" />
         {/* <Map /> */}
       </header>
--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,2 @@
-import { Map } from "./map/map.component";
 import { ChartMap } from "./chart-map/chart-map.component";
-export { Map, ChartMap };
+export { ChartMap };

f135277e8f8ddba948c6241211e6aeb03d0ca3f1
xennia316 <xennia316@gmail.com>
2022-12-13 05:15:44 +0100

    fix: height

--- src/components/cards/about.component.jsx
+++ src/components/cards/about.component.jsx
@@ -3,10 +3,12 @@ import React from "react";
 export const About = ({ data }) => {
   return (
     <div
-      className={`w-full w-full h-fit bg-white rounded-lg px-8 py-4 xl:px-12 xl:py-5 text-gray-500 flex flex-col gap-4`}
+      className={`w-full w-full h-1/3 bg-white rounded-lg px-8 py-4 xl:px-12 xl:py-5 text-gray-500 flex flex-col gap-4`}
     >
       <div className={` text- xl:text-lg`}>About</div>
-      <div className={` text-lg xl:text-lg font-bold`}>{data.bio}</div>
+      <div className={` text-lg xl:text-lg font-bold text-ellipsis`}>
+        {data.bio}
+      </div>
     </div>
   );
 };

6d3461e863a319d4d454a905680c022ab77bdb97
xennia316 <xennia316@gmail.com>
2022-12-22 11:08:37 +0100

    feat: import/export

---
+++ src/assets/images/index.js
@@ -0,0 +1,49 @@
+import map from "./map.png";
+import logo from "./logo.png";
+import code_braces from "./nav_icons/code_braces.png";
+import fitness_center from "./nav_icons/fitness_center.png";
+import rocket_launch from "./nav_icons/rocket_launch.png";
+import trending_up from "./nav_icons/trending_up.png";
+import volunteer_activism from "./nav_icons/volunteer_activism.png";
+import wind_power from "./nav_icons/wind_power.png";
+import triangle_up from "./nav_icons/triangle_up.png";
+import menu_button from "./nav_icons/menu_button.png";
+import german_flag from "./german_flag.png";
+import french_flag from "./french_flag.png";
+import english_flag from "./english_flag.png";
+
+import back_button from "./back_button.png";
+import to_top from "./to_top.png";
+
+import img2 from "./Vector-1.png";
+import img3 from "./Vector-2.png";
+import img4 from "./Vector-3.png";
+import img5 from "./Vector-4.png";
+import drop from "./drop.png";
+import eye from "./eye.png";
+import user_circle from "./user_circle.png";
+
+export {
+  map,
+  code_braces,
+  fitness_center,
+  rocket_launch,
+  trending_up,
+  volunteer_activism,
+  wind_power,
+  triangle_up,
+  menu_button,
+  logo,
+  back_button,
+  to_top,
+  german_flag,
+  french_flag,
+  english_flag,
+  img2,
+  img3,
+  img4,
+  img5,
+  drop,
+  eye,
+  user_circle,
+};
--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,15 @@
 import { ChartMap } from "./chart-map/chart-map.component";
 import { ChartMapExtended } from "./chart-map-extended/chart-map-extended.component";
+import { About } from "./cards/about.component";
+import { SkillCard } from "./cards/skillCard.component";
+import { SocialMediaCard } from "./cards/socialCard.component";
+import { ProfileCard } from "./cards/profileCard.component";
 
-export { ChartMap, ChartMapExtended };
+export {
+  ChartMap,
+  ChartMapExtended,
+  ProfileCard,
+  About,
+  SkillCard,
+  SocialMediaCard,
+};
